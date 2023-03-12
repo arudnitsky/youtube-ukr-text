51ac5140d8a1a7cb1b084a93ea9b743e6ad1563b
Andy Rudnitsky <andy.rudnitsky@gmail.com>
2023-03-12 15:10:20 -0400

    Switch to yt-dlp youtube-dl errors out with 'ERROR: Unable to extract uploader id;'

--- youtube-ukr-text.sh
+++ youtube-ukr-text.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
-youtube-dl -o /tmp/audio.wav --extract-audio --audio-format wav $1 # https://www.youtube.com/watch?v=MXTPXJfWBxA
+yt-dlp -o /tmp/audio.wav --extract-audio --audio-format wav $1 # https://www.youtube.com/watch?v=MXTPXJfWBxA
 [ $? -eq 0 ] && ffmpeg -i /tmp/audio.wav -ac 1 -ar 16000 /tmp/audiomono16khz.wav
 [ $? -eq 0 ] && ukr-speech-to-text.py /tmp/audiomono16khz.wav | tee translated.txt
 [ $? -eq 0 ] && ukrainian-word-stress --symbol combining translated.txt >translatedstressed.txt
-rm translated.txt /tmp/audio.wav /tmp/audiomono16khz.wav 
+rm /tmp/audio.wav /tmp/audiomono16khz.wav 

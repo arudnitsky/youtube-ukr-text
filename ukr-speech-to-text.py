02488327ea8dc094c9d1a88b56e9b89810df479d
Andy Rudnitsky <andy.rudnitsky@gmail.com>
2024-04-05 13:31:27 -0400

    Update to use new environment variables

--- ukr-speech-to-text.py
+++ ukr-speech-to-text.py
@@ -3,14 +3,16 @@
 import os
 import sys
 import time
+
 import azure.cognitiveservices.speech as speechsdk
 
 
 def speech_recognize_continuous_from_file(filename):
     """performs continuous speech recognition with input from an audio file"""
+
     speech_config = speechsdk.SpeechConfig(
-        subscription=os.environ.get("TRANSLATOR_TEXT_RESOURCE_KEY"),
-        region=os.environ.get("TRANSLATOR_TEXT_REGION"),
+        subscription=os.environ.get("SPEECH_KEY"),
+        region=os.environ.get("SPEECH_REGION"),
     )
     speech_config.speech_recognition_language = "uk-UA"
     audio_config = speechsdk.audio.AudioConfig(filename=filename)
@@ -25,7 +27,7 @@ def speech_recognize_continuous_from_file(filename):
         speech_recognizer.stop_continuous_recognition()
         nonlocal done
         done = True
-    
+
     def text_recognized(evt):
         print(evt, file=sys.stderr)
         sys.stderr.flush()
@@ -42,6 +44,7 @@ def speech_recognize_continuous_from_file(filename):
     speech_recognizer.start_continuous_recognition()
     while not done:
         time.sleep(0.5)
+
 
 print(sys.argv[0], sys.argv[1], file=sys.stderr)
 speech_recognize_continuous_from_file(sys.argv[1])
